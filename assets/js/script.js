4f560d0fcea96f06820ec50f3231f75afe4391b5
Sharon <sharonelizalex@gmail.com>
2021-04-07 13:27:24 +1000

    code design logic

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//declare and initialize variables
+
+
+//get search input param and call first weather API.
+
+
+//parse response and populate the frontend fields
+
+
+//call second A[i f or forecast data
+
+
+//function for storasge of city names
+
+//add js event function for button click
