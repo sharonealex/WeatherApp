83212f07eb5783ba473e0b3186741c3425a91852
Sharon <sharonelizalex@gmail.com>
2021-04-08 10:41:54 +1000

    added uv index function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,31 @@ var currentWeatherObj;
 
 
 
-function getCurrentWeather(cityName) { //NOT WORKING AT THE MOMENT
-    var currentWeatherRequest =
-        "https://api.openweathermap.org/data/2.5/weather?q=London&appid=40cb67d75c988d881d5132977c0b65a5";
-    fetch(currentWeatherRequest).then(function (data) {
-        console.log(data);
-    });
+
+
+
+function getUvIndex(lat, lon){
+    console.log("inside uv index function");
+    var uvRequest = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&appid=40cb67d75c988d881d5132977c0b65a5";
+    fetch(uvRequest)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function(jsonResp) {
+        console.log(typeof(jsonResp));
+        console.log(jsonResp.current.uvi)
+        return jsonResp.current.uvi;
+     })
+    .catch(function (error) {
+        console.log('error is' + error);
+    })
 }
 
 
-
-function getUVindex(lat, long){
-    return "7.5"  //functionality to be added later
-}
-
-
-
-function processWeatherData(data) {
+function processCurrentWeatherData(data) {
     var latitude = data.coord.lat;
     var longitude = data.coord.lon;
-    var uvIndex = getUVindex(latitude, longitude);
+    var uvIndex = getUvIndex(latitude, longitude);
     currentWeatherObj = {
         temperature: data.main.temp,
         humidity: data.main.humidity,
@@ -32,48 +37,30 @@ function processWeatherData(data) {
     return currentWeatherObj;
 }
 
-
-function getCurrentWeatherMock() {
-    return {
-        coord: { lon: -0.1257, lat: 51.5085 },
-        weather: [
-            { id: 803, main: "Clouds", description: "broken clouds", icon: "04d" },
-        ],
-        base: "stations",
-        main: {
-            temp: 278.63,
-            feels_like: 274.04,
-            temp_min: 277.59,
-            temp_max: 279.82,
-            pressure: 1024,
-            humidity: 45,
-        },
-        visibility: 10000,
-        wind: { speed: 7.72, deg: 280 },
-        clouds: { all: 75 },
-        dt: 1617792444,
-        sys: {
-            type: 1,
-            id: 1414,
-            country: "GB",
-            sunrise: 1617772914,
-            sunset: 1617820973,
-        },
-        timezone: 3600,
-        id: 2643743,
-        name: "London",
-        cod: 200,
-    };
+function getCurrentWeather(cityName) { 
+    var currentWeatherRequest =
+        "https://api.openweathermap.org/data/2.5/weather?q=London&appid=40cb67d75c988d881d5132977c0b65a5";
+    fetch(currentWeatherRequest)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function(jsonResp) {
+            processCurrentWeatherData(jsonResp)
+         })
+        .catch(function (error) {
+            console.log('error is' + error);
+        })
 }
 
-//Funtion invoking Mocks as APIs not working
+
+
 function getDashboardResults(cityName) {
 
-   var currentWeatherResponse = processWeatherData(getCurrentWeatherMock());
-   displayResponse(currentWeatherResponse);
+   var currentWeatherResponse = processCurrentWeatherData(getCurrentWeather());
+  // displayResponse(currentWeatherResponse);
    
-   var forecast5DaysResponse = processForecastData(getForecastWeatherMock());
-   displayResponse(forecast5DaysResponse)
+   //var forecast5DaysResponse = processForecastData(getForecastWeatherMock());
+  // displayResponse(forecast5DaysResponse)
 
 }
 
