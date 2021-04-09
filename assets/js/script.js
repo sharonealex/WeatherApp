7a0dfbea9e82eb44e87959af2e0dcb9d3b406e28
Sharon <sharonelizalex@gmail.com>
2021-04-09 22:52:53 +1000

    added function to support forecast display

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var windSpeedEl = document.querySelector("#wind-speed");
 var pressureEl = document.querySelector("#pressure");
 var uvindexEl = document.querySelector("#UV-index");
 var uvIndexValue;
+const forecastEl = document.querySelectorAll(".forecast");
 
 
 
@@ -52,6 +53,7 @@ function displayCurrentWeather(city, weather) {
 
 
 
+
 function getDashboardResults(city) {
     var currentWeatherRequest =
         "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=40cb67d75c988d881d5132977c0b65a5";
@@ -72,7 +74,6 @@ function getDashboardResults(city) {
             return getUvIndex(latitude, longitude, currentWeatherObj);
         })
         .then(function () {
-
             return currentWeatherObj;
         })
         .then(function (currentWeatherObj) {
@@ -83,13 +84,54 @@ function getDashboardResults(city) {
         })
 }
 
+function displayForecast(forecastData){
+    for (i=0; i < forecastEl.length; i++) {
+        forecastEl[i].innerHTML = "";
+        const forecastIndex = i*8 + 4;
+        var forecastMonth = '4';
+        var forecastDay = '4';
+        var forecastYear = '5';
+        const forecastDateEl = document.createElement("p");
+        forecastDateEl.setAttribute("class","mt-3 mb-0 forecast-date");
+        forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+        forecastEl[i].append(forecastDateEl);
+        const forecastWeatherEl = document.createElement("img");
+        forecastWeatherEl.setAttribute("src","https://openweathermap.org/img/wn/10d@2x.png");
+        forecastWeatherEl.setAttribute("alt",forecastData.list[0].weather[0].description);
+        forecastEl[i].append(forecastWeatherEl);
+        const forecastTempEl = document.createElement("p");
+        forecastTempEl.innerHTML = "Temp: " + forecastData.list[0].main.temp;
+        forecastEl[i].append(forecastTempEl);
+        const forecastHumidityEl = document.createElement("p");
+        forecastHumidityEl.innerHTML = "Humidity: " + forecastData.list[0].main.humidity + "%";
+        forecastEl[i].append(forecastHumidityEl);
+    }
+}
+
+function get5DaysForeCastDashboardResults(city){
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=london&appid=40cb67d75c988d881d5132977c0b65a5"
+    fetch(forecastUrl)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(forecastData){
+        console.log(forecastData);
+        displayForecast(forecastData)
+    })
+    .catch(function(err){
+        console.log('error'+  err);
+    })
+
+    
+}
+
 
 var searchHandler = function (event) {
     event.preventDefault();
     var city = cityInputEl.value.trim();
     if (city) {
         getDashboardResults(city);
-        get5DaysForecast(city);
+        get5DaysForeCastDashboardResults(city);
         // cities.unshift({city});
         cityInputEl.value = "";
     } else {
